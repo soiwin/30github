46d84a36d40987c49f812b9a42f72a1d3cc1ea1a
soiwin <commercialduck@gmail.com>
2025-02-24 15:45:23 +0600

    day 19

--- day-18/quick_sort.py
+++ day-18/quick_sort.py
@@ -1,7 +1,16 @@
 
 
 def quick_sort(array: list[int]) -> list[int]:
+    if len(array) <= 1:
+        return array
+
     pivot = len(array) // 2
 
-    low = [i for i in range(len(array)) if array[i] < array[pivot]]
+    low = [el for el in array if el < array[pivot]]
+    eq = [el for el in array if el == array[pivot]]
+    high = [el for el in array if el > array[pivot]]
 
+    return quick_sort(low) + eq + quick_sort(high)
+
+
+print(quick_sort([1, 0, 0, -4, 6, -6, 6, 9, 0]))
