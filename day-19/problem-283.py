f6fb7e6b2d52f270881c6e032876846f1bd89b51
soiwin <commercialduck@gmail.com>
2025-02-24 22:30:09 +0600

    day 19.1

---
+++ day-19/problem-283.py
@@ -0,0 +1,14 @@
+
+
+class Solution:
+    def move_zeroes(self, nums: list[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+
+        left = 0
+
+        for right in range(len(nums)):
+            if nums[right] != 0:
+                nums[right], nums[left] = nums[left], nums[right]
+                left += 1        
